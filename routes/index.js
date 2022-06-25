220175b6d6f7a3e10fb960642da6f2aaef722d36
Basudev Singh <bassingh@bassingh-mn1.linkedin.biz>
2022-06-25 12:35:49 +0530

    till sign in

--- routes/index.js
+++ routes/index.js
@@ -5,9 +5,10 @@ const homeController = require('../controllers/home_controllers');
 
 
 router.get('/',homeController.home);
+router.use('/users', require('./users'));
 
-
-
+// for any further routes, access from here
+// router.use('/routerName',require('./routerFile'));
 
 
 module.exports = router;
